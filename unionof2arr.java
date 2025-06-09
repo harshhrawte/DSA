478353a2e39ef84156ca726947988fab64a013ff
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-10 02:17:40 +0530

    added a change

--- unionof2arr.java
+++ unionof2arr.java
@@ -2,13 +2,60 @@ import java.util.*;
 public class unionof2arr{
     public static void main(String[] args)
     {
-     int [] arr1={7,6,9,1};
-     int [] arr2={1,7,5,6,9};
-     optimized(arr1,arr2);
-     intersection(arr1,arr2);
+     int [] arr1={1,2,7,9};
+     int [] arr2={6,7,7,8,9};
+     //better(arr1,arr2);
+     //intersection(arr1,arr2);
+     System.out.println(optimized(arr1, arr2));
     }
 
-    static int  optimized(int []nums1,int []nums2)
+
+static ArrayList<Integer> optimized(int [] nums1,int [] nums2)
+{
+    ArrayList<Integer> union=new ArrayList<>();
+    int i=0;
+    int j=0;
+    int m=nums1.length;
+    int n=nums2.length;
+    while(i<m && j<n)
+    {
+       if(nums1[i]<=nums2[j])
+       {
+         if(union.size()==0 || union.get(union.size()-1)!=nums1[i]){
+            union.add(nums1[i]);
+         } 
+         i++;
+       }
+       else {
+       if(union.size()==0 || union.get(union.size()-1)!=nums2[j]){
+         union.add(nums2[j]);
+       }
+       j++;
+    }
+    }
+
+    while(i<m)
+    {
+       if(union.get(union.size()-1) !=nums1[i])
+       {
+        union.add(nums1[i]);
+       }
+       i++;
+    }
+    while(j<n)
+    {
+     if(union.get(union.size()-1) !=nums2[j])
+       {
+        union.add(nums2[j]);
+       }
+       j++;   
+    }
+    return union;
+}
+
+}
+
+    /*static int  better(int []nums1,int []nums2)
     {
         HashSet<Integer> set=new HashSet<>();
         for(int i=0;i<nums1.length;i++)
@@ -45,4 +92,9 @@ public class unionof2arr{
         return count;
     }
 
-}
+}*/
+
+
+
+
+
