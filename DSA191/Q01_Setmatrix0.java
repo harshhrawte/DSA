47f6cd67a2458a41104355ca6f225a481a19bc0c
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-25 01:22:23 +0530

    1st done

--- DSA191/Q01_Setmatrix0.java
+++ DSA191/Q01_Setmatrix0.java
@@ -9,16 +9,31 @@ public class Q01_Setmatrix0 {
          {
              System.out.println(arr[i][j]+ "");
          }
+         System.out.println();
         }
+
         brute(arr);
-        System.out.println("Array after set matrix 0");
+        System.out.println("Array after set matrix 0 brute");
           for(int i=0;i<arr.length;i++)
         {
          for(int j=0;j<arr[0].length;j++)
          {
              System.out.print(arr[i][j] + "");
          }
+         System.out.println();
         }
+
+        optimized(arr);
+          System.out.println("Array after set matrix 0 optimized");
+          for(int i=0;i<arr.length;i++)
+        {
+         for(int j=0;j<arr[0].length;j++)
+         {
+             System.out.print(arr[i][j] + "");
+         }
+         System.out.println();
+        }
+
     }
 
 
@@ -56,7 +71,76 @@ static void brute(int [] [] nums)
 //optimized
 static void optimized(int [] [] nums)
 {
-   
+   boolean firstrow=false;
+   boolean firstcol=false;
+   int n=nums.length;
+   int m=nums[0].length;
+
+
+   //row
+   for(int j=0;j<m;j++)
+   {
+     if(nums[0][j]==0)
+     {
+        firstrow=true;
+     }
+   }
+
+
+   //column
+   for(int i=0;i<n;i++)
+   {
+     if(nums[i][0]==0)
+     {
+        firstcol=true;
+     }
+   }
+
+   //rest matrix 0
+   for(int i=0;i<n;i++)
+   {
+    for(int j=0;j<m;j++)
+    {
+       if(nums[i][j]==0)
+       {
+        nums[0][j]=0;
+        nums[i][0]=0;
+       }
+    }
+   }
+
+
+//set matrix
+   for(int i=0;i<n;i++)
+   {
+    for(int j=0;j<m;j++)
+    {
+       if(nums[i][0]==0 || nums[0][j]==0)
+       {
+        nums[i][j]=0;
+       }
+    }
+   }
+
+
+   //firstrow
+   if(firstrow)
+   {
+    for(int j=0;j<m;j++)
+    {
+        nums[0][j]=0;
+    }
+   }
+
+
+   //firstcolumn
+    if(firstcol)
+   {
+    for(int i=0;i<n;i++)
+    {
+        nums[i][0]=0;
+    }
+   }
 }
 }
 
