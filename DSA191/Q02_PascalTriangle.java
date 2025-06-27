8bb47bb872fad3e8349e901811479badff615e58
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-28 01:10:54 +0530

    final commit

--- DSA191/Q02_PascalTriangle.java
+++ DSA191/Q02_PascalTriangle.java
@@ -1,20 +1,18 @@
 package DSA191; 
-import java.util.ArrayList; // Importing ArrayList class from java.util package.
-import java.util.List;      // Importing List interface to use List<List<Integer>>.
+import java.util.ArrayList; 
+import java.util.List;      
 
-public class Q02_PascalTriangle { // Class declaration.
+public class Q02_PascalTriangle { 
 
-    public static void main(String[] args) { // Main method: program starts here.
-        int numRows = 5; // You can change this to generate more rows.
-        List<List<Integer>> triangle = generate(numRows); // Calling method to generate the triangle.
-        
-        // Printing the triangle from the stored list
+    public static void main(String[] args) { 
+        int numRows = 5; 
+        List<List<Integer>> triangle = generate(numRows); 
+      
         for (List<Integer> row : triangle) {
             System.out.println(row);
         }
     }
 
-    // This method builds Pascal’s Triangle and stores it in a list of lists
    static List<List<Integer>>generate(int numRows)
    {
        List<List<Integer>>result =new ArrayList<>();
