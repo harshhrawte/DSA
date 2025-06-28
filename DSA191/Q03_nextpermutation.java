26e83c0583bc5dbe7cf3fc1cb8605b70a8c90c9e
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-28 12:37:57 +0530

    Final Commit

--- DSA191/Q03_nextpermutation.java
+++ DSA191/Q03_nextpermutation.java
@@ -1,6 +1,39 @@
 package DSA191;
 class Q03_nextpermutation{
 public static void main(String[] args) {
-    
+    int [] nums={2,1,5,4,0,0};
+    System.out.println("Array before the next permutation :");
+    for(int i=0;i<nums.length;i++)
+    { 
+       System.out.println(nums[i]);
+        }       
+}
+
+//brute
+static int brute(int [] nums)
+{
+    int result=0;
+
+    return result;
+}
+
+//optimized
+static int optimized(int [] nums)
+{
+    int result=0;
+
+    return result;
+}
+
+private void reverse(int [] nums,int start,int end)
+{
+    while(start<end)
+    {
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+    }
 }
 }
