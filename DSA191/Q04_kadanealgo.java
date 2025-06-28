6c6be4c15ba191fe033ad7f37c92dd682554d3c2
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-28 18:34:26 +0530

    Final Commit

---
+++ DSA191/Q04_kadanealgo.java
@@ -0,0 +1,28 @@
+package DSA191;
+class Q04_kadanealgo{
+public static void main(String[] args) {
+    int [] nums={1,2,5,6,8,9};
+    System.out.println("The Array is :");
+    for(int i=0;i<nums.length;i++)
+    {
+        System.out.println(nums[i]);
+    }
+}
+
+
+//brute
+static int brute(int [] nums)
+{
+    int x=0;
+    return x;
+}
+
+
+//optimized
+static int optimized(int [] nums)
+{
+    int x=0;
+    return x;
+}
+
+}
