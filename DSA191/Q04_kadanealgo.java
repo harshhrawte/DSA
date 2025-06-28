c87fb0fc46691b606e3a315981b6d9a85d7c8324
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-06-28 19:47:28 +0530

    Final Commit

--- DSA191/Q04_kadanealgo.java
+++ DSA191/Q04_kadanealgo.java
@@ -1,28 +1,49 @@
 package DSA191;
 class Q04_kadanealgo{
 public static void main(String[] args) {
-    int [] nums={1,2,5,6,8,9};
-    System.out.println("The Array is :");
-    for(int i=0;i<nums.length;i++)
-    {
-        System.out.println(nums[i]);
-    }
+     int [] nums={1,4,-2,-4,0};
+    System.out.println(brute(nums, 5));
+    System.out.println(optimized(nums,5));
 }
 
 
 //brute
-static int brute(int [] nums)
+static int brute(int [] nums,int l)
 {
-    int x=0;
-    return x;
+   long maxi=Integer.MIN_VALUE;
+   for(int i=0;i<l;i++)
+   {
+    for(int j=i;j<l;j++)
+    {
+        int sum=0;
+      for(int k=i;k<j;k++)
+      {
+        sum+=nums[k];
+      }
+    maxi=(int)Math.max(sum,maxi);
+    }
+   }
+   return (int)maxi;
 }
 
-
 //optimized
-static int optimized(int [] nums)
+static int optimized(int [] nums,int n)
 {
-    int x=0;
-    return x;
+     long maxi=Integer.MIN_VALUE;
+     int sum=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        sum+=nums[i];
+        if(sum>maxi)
+        {
+            maxi=sum;
+        }
+        if(sum<0)
+        {
+            sum=0;
+        }
+     }
+     return (int)maxi;
 }
 
 }
